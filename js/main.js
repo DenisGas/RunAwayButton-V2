b618a6b010ac82e504d57dd952a1cd1deedba00a
DenisGas <dehuc1811@gmail.com>
2022-05-08 17:47:44 +0300

    Added lets and function
    Added function startGame (  launch game animation )

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ const startBtn = document.querySelector("#start");
 const start = document.querySelector(".start");
 const game = document.querySelector(".spd-scr-btn");
 const gameStart = document.querySelector(".game");
+
+let lastSpeed;
+let btnSpeed;
+let speed = 'min';
+let score = 0;
+let minScore = 0;
+let midScore = 0;
+let maxScore = 0;
+let i = 0;
+let index = 0;
+
+startGame()
+
+
+function startGame(){
+  setTimeout(() => {
+      gameStart.style.opacity = 1;
+      gameStart.style.transition = "2s";
+    }, 500)
+};
