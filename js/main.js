113ac7e7dd7de350eaceb8af8715bfea013614b1
DenisGas <dehuc1811@gmail.com>
2022-05-08 17:39:10 +0300

    Added const in main.js
    Added const for colors and  adding element from DOM

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const YELLOW = "#EBCB8B";
+const BLUE = "#5E81AC";
+const GREEN = "#A3BE8C";
+const colors = [YELLOW, BLUE , GREEN];
+const btn = document.querySelector("#no-catch");
+const scoreBtn = document.querySelector("#score-button");
+const speedBtn = document.querySelector("#speed-button");
+const clickScoreSpan = document.querySelector("#score");
+const speedSpan = document.querySelector("#speed");
+const startBtn = document.querySelector("#start");
+const start = document.querySelector(".start");
+const game = document.querySelector(".spd-scr-btn");
+const gameStart = document.querySelector(".game");
